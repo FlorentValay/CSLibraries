880b7513eeb7193c23d9db3e26bd1fba6c370d40
Florent Valay <fragueurfou@live.fr>
2019-03-28 14:35:22 +0100

    Sorting Tests

---
+++ CSLibrariesTest/Structures/List/ToStringListTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSLibrariesTest.Structures.List
+{
+    /// <summary>
+    /// Test class for CSLibrariesTest.Structures.List.ToString()
+    /// </summary>
+    [TestClass]
+    public class ToStringListTest
+    {
+        [TestMethod]
+        public void ToString_Empty_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ToString_Semifull_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ToString_Full_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToString_AfterClear_Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
