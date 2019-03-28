b6669241918343356c9b7653b7562ab7492f189d
Florent Valay <fragueurfou@live.fr>
2019-03-28 18:47:20 +0100

    Begin of test on List[]

--- CSLibrariesTest/Structures/List/thisListTest.cs
+++ CSLibrariesTest/Structures/List/thisListTest.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using CSLibraries.Structures;
+﻿using CSLibraries.Structures;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CSLibrariesTest.Structures.List
 {
+    /// <summary>
+    /// Test class for CSLibrariesTest.Structures.List[].
+    /// </summary>
     [TestClass]
-    public class thisListTest
+    public class ThisListTest
     {
         // TODO: Coder la suite de tests
+
+        /// <summary>
+        /// This test atempt to see if a negative index throws an exception of type System.IndexOutOfRangeException.
+        /// </summary>
+        [TestMethod]
+        public void This_OutOfMinRange_ThrowException()
+        {
+            List<int> list = new List<int>();
+
+            Assert.ThrowsException<System.IndexOutOfRangeException>(() => list[-1]);
+        }
     }
 }
