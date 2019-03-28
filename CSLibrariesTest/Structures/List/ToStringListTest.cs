30bb7a89e9cc765f335ea9d014239a3698efadda
Florent Valay <fragueurfou@live.fr>
2019-03-28 14:48:44 +0100

    Adding List.ToString() Tests

--- CSLibrariesTest/Structures/List/ToStringListTest.cs
+++ CSLibrariesTest/Structures/List/ToStringListTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CSLibraries.Structures;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CSLibrariesTest.Structures.List
@@ -9,27 +10,61 @@ namespace CSLibrariesTest.Structures.List
     [TestClass]
     public class ToStringListTest
     {
+        /// <summary>
+        /// This test atempt to see if ToString function will return "| |" after clearing a list.
+        /// </summary>
+        [TestMethod]
+        public void ToString_AfterClear_Test()
+        {
+            List<string> list = new List<string>();
+
+            list.Add("a");
+            list.Add("a");
+            list.Add("a");
+            list.Clear();
+
+            Assert.AreEqual("| |", list.ToString(), $"Expected '| |' and recieve '{list.ToString()}'");
+        }
+
+        /// <summary>
+        /// This test atempt to see if ToString function will return "| |" for empty list.
+        /// </summary>
         [TestMethod]
         public void ToString_Empty_Test()
         {
-            throw new NotImplementedException();
+            List<string> list = new List<string>();
+
+            Assert.AreEqual("| |", list.ToString(), $"Expected '| |' and recieve '{list.ToString()}'");
         }
 
-        [TestMethod]
-        public void ToString_Semifull_Test()
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// This test atempt to see if ToString function will return the true content of a full list with mixed normal and default values.
+        /// </summary>
         [TestMethod]
         public void ToString_Full_Test()
         {
-            throw new NotImplementedException();
+            List<string> list = new List<string>(3);
+
+            list.Add("a");
+            list.Add(default(string));
+            list.Add("a");
+
+            Assert.AreEqual("| a |  | a |", list.ToString(), $"Expected '| a |  | a |' and recieve '{list.ToString()}'");
         }
 
-        public void ToString_AfterClear_Test()
+        /// <summary>
+        /// This test atempt to see if ToString function will return the true content of a semifull list with mixed normal and default values.
+        /// </summary>
+        [TestMethod]
+        public void ToString_Semifull_Test()
         {
-            throw new NotImplementedException();
+            List<string> list = new List<string>();
+
+            list.Add("a");
+            list.Add(default(string));
+            list.Add("a");
+
+            Assert.AreEqual("| a |  | a |", list.ToString(), $"Expected '| a |  | a |' and recieve '{list.ToString()}'");
         }
     }
 }
