f6ad433004c4f7abba2f2d5abf2fb7317e6c3510
Florent Valay <fragueurfou@live.fr>
2019-03-29 19:01:53 +0100

    Creating test for List.RemoveAt

--- CSLibrariesTest/Structures/List/RemoveAtListTest.cs
+++ CSLibrariesTest/Structures/List/RemoveAtListTest.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using CSLibraries.Structures;
+﻿using CSLibraries.Structures;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CSLibrariesTest.Structures.List
 {
+    /// <summary>
+    /// Test class for List.RemoveAt()
+    /// </summary>
+    [TestClass]
     public class RemoveAtListTest
     {
         // TODO: Coder la suite de tests
+
+        /// <summary>
+        /// This test attempts to see if trying to remove at a negative index will throw an exception
+        /// </summary>
+        [TestMethod]
+        public void RemoveAt_OverMinRange_ThrowsException()
+        {
+            List<int> list = new List<int>();
+
+            Assert.ThrowsException<System.IndexOutOfRangeException>(() => list.RemoveAt(-1));
+        }
     }
 }
